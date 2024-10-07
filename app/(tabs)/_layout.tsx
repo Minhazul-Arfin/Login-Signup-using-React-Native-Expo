0d57877fff389cdfbad08256c0b077af6568e535
Minhazul-Arfin <arefinminhaz45@gmail.com>
2024-10-07 15:38:19 +0600

    search updated, settings added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,17 +6,23 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 const TabsLayout = () => {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: '#10b981' }}>
-      <Tabs.Screen name='info' options={{title:"Info",
-                                            tabBarIcon: ({ color }) => 
-                                              <FontAwesome size={28} name="users" color={color} />,
-        }}/>
       <Tabs.Screen name='index' options={{title:"Home", 
                                             tabBarIcon: ({ color }) => 
                                             <FontAwesome size={28} name="home" color={color} />,}}/>
+
+      <Tabs.Screen name='info' options={{title:"Peoples",
+                                            tabBarIcon: ({ color }) => 
+                                              <FontAwesome size={28} name="users" color={color} />,
+        }}/>
+      
         
+      <Tabs.Screen name='search'  options={{title:"Search",
+                                            tabBarIcon: ({ color }) => 
+                                              <FontAwesome size={28} name="search" color={color} />,}}/>
+      
       <Tabs.Screen name='settings'  options={{title:"Settings",
                                             tabBarIcon: ({ color }) => 
-                                              <FontAwesome size={28} name="gears" color={color} />,}}/>
+                                              <FontAwesome size={28} name="wrench" color={color} />,}}/>
     </Tabs>
   )
 }
